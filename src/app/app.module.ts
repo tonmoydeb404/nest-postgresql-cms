ae9b4e99edc049cb81422fa5e0715b8c93a32c8d
tonmoydeb404 <tonmoydeb404@gmail.com>
2025-04-19 17:19:12 +0600

    moved app module files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
 import { UsersModule } from 'src/users/users.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controllers/app.controller';
 import { GlobalModule } from './global.module';
+import { AppService } from './services/app.service';
 
 @Module({
   imports: [GlobalModule, UsersModule, AuthModule],
